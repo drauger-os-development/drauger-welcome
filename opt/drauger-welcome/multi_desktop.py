98855adf1f978d31ca5cb061f01ca5ad1fd43a7a
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-01-29 01:42:15 -0500

    finished fixing window pile-up bug

--- opt/drauger-welcome/multi_desktop.py
+++ opt/drauger-welcome/multi_desktop.py
@@ -8,11 +8,15 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, Pango
 from ctypes import cdll, byref, create_string_buffer
 
-message_show=""
+message_show="""
+  Having multiple desktops is the ability to switch back and forth between two "desktops."  
+  This ability makes it so that you can hide windows and tabs for apps on one desktop while you work in another.  
+  """
 
 class notify(Gtk.Window):
 	
 	def __init__(self):
+		self.check=0
 		Gtk.Window.__init__(self, title="Drauger OS Tutorial")
 		self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 		self.add(self.box)
@@ -25,9 +29,22 @@ class notify(Gtk.Window):
 		self.button1 = Gtk.Button.new_with_label(label="Next")
 		self.button1.connect("clicked", self.onnextclicked)
 		self.box.pack_start(self.button1, True, True, 0)
-        
+
 	def onnextclicked(self, button):
-		Gtk.main_quit()
+		if self.check == 0:
+			self.label.set_text("""
+  This allows for greater organization, privacy, control,  
+  and productivity.  
+  """)
+		elif self.check == 1:
+			self.label.set_text("""
+  To switch from one desktop to another, click on the rectangle that is grey between the two  
+  on the far right of the top desktop panel.  
+  """)
+		elif self.check == 2:
+			self.hide()
+			exit()
+		self.check=self.check+1
 
 def notify_show():
 	window = notify()
@@ -41,18 +58,4 @@ def notify_show():
 	Gtk.main() 
 	window.connect("delete-event", Gtk.main_quit)
 
-message_show="""
-  Having multiple desktops is the ability to switch back and forth between two "desktops."  
-  This ability makes it so that you can hide windows and tabs for apps on one desktop while you work in another.  
-  """
-notify_show()
-message_show="""
-  This allows for greater organization, privacy, control,  
-  and productivity.  
-  """
-notify_show()
-message_show="""
-  To switch from one desktop to another, click on the rectangle that is grey between the two  
-  on the far right of the top desktop panel.  
-  """
 notify_show()
