7060b3b934e36e7fb2c5ec7f960164b956058a74
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-01-29 01:35:54 -0500

    bug fix
    fixed window pile bug and bug where only the first window would show

--- opt/drauger-welcome/tut.py
+++ opt/drauger-welcome/tut.py
@@ -27,11 +27,102 @@ from gi.repository import Gtk, Gdk, Pango
 from ctypes import cdll, byref, create_string_buffer
 from os import system
 
-message_show=""
+message_show="""
+  Thank you for downloading and installing Drauger OS, the free, open-sourced, Linux gaming OS.  
+  """
 
+class Menu_tut1(Gtk.Window):
+
+	def __init__(self):
+		Gtk.Window.__init__(self, title="Drauger OS Tutorial")
+
+		self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		self.add(self.box)
+
+		self.label = Gtk.Label()
+		self.label.set_markup("""
+  Would you like to learn more about multiple desktops?  
+  """)
+		self.label.set_justify(Gtk.Justification.CENTER)
+		self.box.pack_start(self.label, True, True, 0)
+
+		self.button1 = Gtk.Button.new_with_label(label="Yes")
+		self.button1.connect("clicked", self.onyesclicked)
+		self.box.pack_start(self.button1, True, True, 0)
+
+		self.button2 = Gtk.Button.new_with_label(label="No")
+		self.button2.connect("clicked", self.onnoclicked)
+		self.box.pack_start(self.button2, True, True, 0)
+
+	def onyesclicked(self, button):
+		self.hide()
+		system("python3 /opt/drauger-welcome/multi_desktop.py")
+		tutorial_menu2()
+		
+	def onnoclicked(self, button):
+		self.hide()
+		tutorial_menu2()
+		
+class Menu_tut2(Gtk.Window):
+
+	def __init__(self):
+		Gtk.Window.__init__(self, title="Drauger OS Tutorial")
+
+		self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		self.add(self.box)
+
+		self.label = Gtk.Label()
+		self.label.set_markup("""
+  Thank you again for using Drauger OS. Would you like to uninstall this tutorial?  
+  """)
+		self.label.set_justify(Gtk.Justification.CENTER)
+		self.box.pack_start(self.label, True, True, 0)
+
+		self.button1 = Gtk.Button.new_with_label(label="Yes")
+		self.button1.connect("clicked", self.onyesclicked)
+		self.box.pack_start(self.button1, True, True, 0)
+
+		self.button2 = Gtk.Button.new_with_label(label="No")
+		self.button2.connect("clicked", self.onnoclicked)
+		self.box.pack_start(self.button2, True, True, 0)
+
+	def onyesclicked(self, button):
+		self.hide()
+		system("/opt/drauger-welcome/u.sh")
+		exit()
+
+	def onnoclicked(self, button):
+		self.hide()
+		exit()
+
+def tutorial_menu1():
+	window = Menu_tut1()
+	window.set_decorated(True)
+	window.set_resizable(True)
+	window.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1,1,1,1))
+	window.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse("black"))
+	window.set_opacity(0.0)
+	window.set_position(Gtk.WindowPosition.CENTER)
+	window.show_all()
+	Gtk.main() 
+	window.connect("delete-event", Gtk.main_quit)
+	
+def tutorial_menu2():
+	window = Menu_tut2()
+	window.set_decorated(True)
+	window.set_resizable(True)
+	window.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1,1,1,1))
+	window.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse("black"))
+	window.set_opacity(0.0)
+	window.set_position(Gtk.WindowPosition.CENTER)
+	window.show_all()
+	Gtk.main() 
+	window.connect("delete-event", Gtk.main_quit)
+	
 class notify(Gtk.Window):
 	
 	def __init__(self):
+		self.check=0
 		Gtk.Window.__init__(self, title="Drauger OS Tutorial")
 		self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 		self.add(self.box)
@@ -44,9 +135,49 @@ class notify(Gtk.Window):
 		self.button1 = Gtk.Button.new_with_label(label="Next")
 		self.button1.connect("clicked", self.onnextclicked)
 		self.box.pack_start(self.button1, True, True, 0)
-        
+			
 	def onnextclicked(self, button):
-		self.hide()
+		if self.check == 0:
+			self.label.set_text("""
+  In this tutorial, you will recive a quick introduction into how to work Drauger OS.  
+  """)
+		elif self.check == 1:
+			self.label.set_text("""
+The two bars in the middle of your screen are your desktop panels. 
+  The top contains quick access to all the most commonly used apps and widgets.   
+The bottom one contains the buttons for the windows you currently have open, as well as
+buttons for things such as shuting down, logging out, etc.
+""")
+		elif self.check == 2:
+			self.label.set_text("""
+  In order to see any apps in the top desktop panel that are not  
+  currently on display, simply click on the drop down arrow underneath  
+  the icon  
+  """)
+		elif self.check == 3:
+			self.label.set_text("""
+  In order to see any apps not on the top desktop panel, click the Drauger OS Logo
+  on the far left of the panel.  
+  """)
+		elif self.check == 4:
+			self.label.set_text("""
+  Drauger OS also has support for not only keyboards and mice,  
+  as well as touchpads and touchscreens,  
+  it also has support for most Xbox and Xbox 360 controllers,  
+  as well as some Playstation controllers. 
+  """)
+		elif self.check == 5:
+			self.label.set_text("""
+  For more info on gamepad support, please go to the command line and run `xboxdrv --help-devices`  
+  """)
+		elif self.check == 6:
+			self.label.set_text("""
+  Finally, the four rectangles on the far right of the top desktop panel are the four current desktops.  
+  """)
+		elif self.check == 7:
+			self.hide()
+			tutorial_menu1()
+		self.check=self.check+1
 
 def notify_show():
 	window = notify()
@@ -60,101 +191,4 @@ def notify_show():
 	Gtk.main() 
 	window.connect("delete-event", Gtk.main_quit)
 	
-class Menu_tut(Gtk.Window):
-
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Drauger OS Tutorial")
-        
-        self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(self.box)
-        
-        self.label = Gtk.Label()
-        self.label.set_markup(message_show)
-        self.label.set_justify(Gtk.Justification.CENTER)
-        self.box.pack_start(self.label, True, True, 0)
-        
-        self.button1 = Gtk.Button.new_with_label(label="Yes")
-        self.button1.connect("clicked", self.onyesclicked)
-        self.box.pack_start(self.button1, True, True, 0)
-        
-        self.button2 = Gtk.Button.new_with_label(label="No")
-        self.button2.connect("clicked", self.onnoclicked)
-        self.box.pack_start(self.button2, True, True, 0)
-        
-    def onyesclicked(self, button):
-        self.hide()
-        Menu_tut.x=0
-        return(Menu_tut.x)
-
-    def onnoclicked(self, button):
-        self.hide()
-        Menu_tut.x=1
-        return(Menu_tut.x)
-
-def tutorial_menu():
-	window = Menu_tut()
-	window.set_decorated(True)
-	window.set_resizable(True)
-	window.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1,1,1,1))
-	window.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse("black"))
-	window.set_opacity(0.0)
-	window.set_position(Gtk.WindowPosition.CENTER)
-	window.show_all()
-	Gtk.main() 
-	window.connect("delete-event", Gtk.main_quit)
-	return(Menu_tut.x)
-
-message_show="""
-  Thank you for downloading and installing Drauger OS, the free, open-sourced, Linux gaming OS.  
-  """
 notify_show()
-message_show="""
-  In this tutorial, you will recive a quick introduction into how to work Drauger OS.  
-  """
-notify_show()
-message_show="""
-The two bars in the middle of your screen are your desktop panels. 
-  The top contains quick access to all the most commonly used apps and widgets.   
-The bottom one contains the buttons for the windows you currently have open, as well as
-buttons for things such as shuting down, logging out, etc.
-"""
-notify_show()
-message_show="""
-  In order to see any apps in the top desktop panel that are not  
-  currently on display, simply click on the drop down arrow underneath  
-  the icon  
-  """
-notify_show()
-message_show="""
-  In order to see any apps not on the top desktop panel, click the Drauger OS Logo
-  on the far left of the panel.  
-  """
-notify_show()
-message_show="""
-  Drauger OS also has support for not only keyboards and mice,  
-  as well as touchpads and touchscreens,  
-  it also has support for most Xbox and Xbox 360 controllers,  
-  as well as some Playstation controllers. 
-  """
-notify_show()
-message_show="""
-  For more info on gamepad support, please go to the command line and run `xboxdrv --help-devices`  
-  """
-notify_show()
-message_show="""
-  Finally, the four rectangles on the far right of the top desktop panel are the four current desktops.  
-  """
-notify_show()
-message_show="""
-  Would you like to learn more about multiple desktops?  
-  """
-x=tutorial_menu()
-if x==0:
-	system("python3 /opt/drauger-welcome/multi_desktop.py")
-message_show="""
-  Thank you again for using Drauger OS. Would you like to uninstall this tutorial?  
-  """
-x=tutorial_menu()
-if x==0:
-	system("/opt/drauger-welcome/u.sh")
-
