0786470616fd599b5739814deb749d96d6aae2c6
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-03-23 13:26:13 -0400

    fix build bug

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ VERSION=$(cat DEBIAN/control | grep 'Version: ' | sed 's/Version: //g')
 PAK=$(cat DEBIAN/control | grep 'Package: ' | sed 's/Package: //g')
 mkdir ../"$PAK"_"$VERSION"_all
 cp -R usr ../"$PAK"_"$VERSION"_all/usr
-cp -R lib ../"$PAK"_"$VERSION"_all/opt
+cp -R etc ../"$PAK"_"$VERSION"_all/etc
 cp -R DEBIAN ../"$PAK"_"$VERSION"_all/DEBIAN
 cd ..
 dpkg-deb --build "$PAK"_"$VERSION"_all
