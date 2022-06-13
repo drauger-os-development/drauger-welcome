2b26db593b96a39d4ccc1d948f418b27bf549314
Thomas Castleman <contact@draugeros.org>
2022-06-13 00:11:43 -0400

    further work for alert window

--- usr/share/drauger-welcome/tut.py
+++ usr/share/drauger-welcome/tut.py
@@ -26,7 +26,7 @@ import sys
 from os import path, getenv
 import subprocess
 import csv
-import datetime
+import time
 import drauger_welcome.main_ui as main_ui
 
 # Make it easier for us to print to stderr
@@ -34,12 +34,36 @@ def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
 
 
-def make_ints(string_list):
+def make_ints(string_list: str):
 	"""make a list of strings into a list of ints"""
 	new_list = []
 	for each in string_list:
 		new_list.append(int(each))
 	return new_list
+
+def is_before(before: list, after: list):
+	"""Check if a given date list comes before another one
+	
+	Return True if `before` comes before `after`.
+	Return None if they are the same date.
+	Return False if `after` comes before `before`.
+	"""
+	if before[0] < after[0]: # check year first
+		return True
+	if before[0] > after[0]:
+		return False
+	# if we get to here we KNOW `before[0]` == `after[0]`
+	if before[1] < after[1]: # check month second
+		return True
+	if before[1] > after[1]:
+		return False
+	# finally, if we get here, we know `before` and `after` are in the same month of the same year
+	if before[2] < after[2]: # check day last
+		return True
+	if before[2] > after[2]:
+		return False
+	# if we get here, `before` == `after`. Return None.
+	return None
 
 
 HOME = getenv("HOME")
@@ -64,8 +88,31 @@ for each in range(len(csv_reader) - 1, -1, -1):
 		current_time = make_ints(current_time)
 		eol_time = csv_reader[each][-1].split("-")
 		eol_time = make_ints(eol_time)
+		add_time = 1209600 # number of seconds in 14 days
 		# find dismantle time (eol_time + 14 days)
+		dismantle_time = time.strftime("%Y-%m-%d", time.gmtime(time.time() + dismantle_time))
+		dismantle_time = dismantle_time.split("-")
+		dismantle_time = make_ints(dismantle_time)
 		# find alert time (eol_time - 14 days)
+		alert_time = time.strftime("%Y-%m-%d", time.gmtime(time.time() - dismantle_time))
+		alert_time = alert_time.split("-")
+		alert_time = make_ints(alert_time)
+		# check for alerts in this order:
+		# alert_time -> eol_time -> dismantle_time
+		alert = not is_before(current_time, alert_time)
+		if alert: # not None evaluates to True
+			eol = not is_before(current_time, eol_time)
+			if eol:
+				dismantle = not is_before(current_time, dismantle_time)
+				if dismantle:
+					# display dismantled dialog - Let the user know that this version of Drauger OS can no longer update without
+					# modifications to /etc/apt/sources.list
+					break
+				# if we get here then this version of Drauger OS is EOL but not yet dismantled, alert user they can still upgrade to
+				# the next version of Drauger OS
+				break
+			# if we get here then this version of Drauger OS is within 2 weeks of being EOL. Alert the user.
+			break
 if ((not path.exists(HOME + "/.drauger-tut")) and (not path.exists("/etc/system-installer/oem-post-install.flag"))):
 	try:
 		main_ui.welcome_show()
