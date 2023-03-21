a96319a760eabc7bc25dd1bd22f29af3db238328
Thomas Castleman <contact@draugeros.org>
2023-03-21 14:12:43 -0400

    Revert "Work to add EOL warning next time this occurs"
    This reverts commit 786058f4a5ebdf1d1f995520ecd3ccbe22b8bd04.

--- usr/share/drauger-welcome/tut.py
+++ usr/share/drauger-welcome/tut.py
@@ -24,23 +24,12 @@
 from __future__ import print_function
 import sys
 from os import path, getenv
-import subprocess
-import csv
-import datetime
+from subprocess import Popen
 import drauger_welcome.main_ui as main_ui
 
 # Make it easier for us to print to stderr
 def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
-
-
-def make_ints(string_list):
-	"""make a list of strings into a list of ints"""
-	new_list = []
-	for each in string_list:
-		new_list.append(int(each))
-	return new_list
-
 
 HOME = getenv("HOME")
 # check if system-installer will be running, if it is, do not show the welcome screen
@@ -49,27 +38,10 @@ with open("/proc/cmdline", "r") as cmdline_file:
 if "system-installer" in cmdline:
             # Not wanted to be running ootb
             sys.exit(0)
-# check to see if we are going to be EOL soon
-file = open("/usr/share/distro-info/Draugeros.csv", "r")
-csv_reader = list(csv.reader(file))
-version = subprocess.check_output(["lsb_release", "-rs"]).decode()[:-1]
-
-# we want to iterate BACKWARDS because the version number is likely to be either the
-# last line, or the next-to-last line. This means our big-O stays the same, but our
-# true execution time is SIGNIFICANTLY reduced.
-for each in range(len(csv_reader) - 1, -1, -1):
-	if csv_reader[each][0] == version:
-		current_time = time.strftime("%Y-%m-%d", time.gmtime())
-		current_time = current_time.split("-")
-		current_time = make_ints(current_time)
-		eol_time = csv_reader[each][-1].split("-")
-		eol_time = make_ints(eol_time)
-		# find dismantle time (eol_time + 14 days)
-		# find alert time (eol_time - 14 days)
 if ((not path.exists(HOME + "/.drauger-tut")) and (not path.exists("/etc/system-installer/oem-post-install.flag"))):
 	try:
 		main_ui.welcome_show()
 	except Exception as e:
-		subprocess.Popen("/usr/share/drauger-welcome/log-out 2 /usr/share/drauger-welcome/tut.py \"Error: %s . main_ui.py has failed.\"" % (e))
+		Popen("/usr/share/drauger-welcome/log-out 2 /usr/share/drauger-welcome/tut.py \"Error: %s . main_ui.py has failed.\"" % (e))
 else:
 	exit(2)
